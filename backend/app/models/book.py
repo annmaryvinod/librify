63345e6fe9c0dadbf59258e239bc665b7eafa089
Annmary Vinod <annmarypriyavinod@gmail.com>
2025-12-07 00:56:07 +0530

    code cleanup

--- backend/app/models/book.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Date
-from app.config.database import Base #to create the database models
-
-class Book(Base):
-    __tablename__ = "books"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False, index=True)
-    author = Column(String, nullable=False)
-    language = Column(String, nullable=False)
